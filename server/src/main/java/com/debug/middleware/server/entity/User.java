9e8d49e4f20fe54ecd9c6d8be0a43628e4ab8c2c
luokun <luokunwork@163.com>
2021-04-22 09:36:26 +0800

    分布式中间件

--- server/src/main/java/com/debug/middleware/server/entity/User.java
+++ server/src/main/java/com/debug/middleware/server/entity/User.java
@@ -21,6 +21,30 @@ public class User {
     public User() {
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public User(Integer id, String userName, String name) {
         this.id = id;
         this.userName = userName;
